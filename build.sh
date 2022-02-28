9f1fd22f36e7dfbd3217172c21a0a9723e159e23
Jonas Stendahl <jonas.stendahl@gmail.com>
2022-02-28 15:50:53 +0100

    Add -trimpath to build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ build() {
   OUTPUT_DIR="intro-detection-info_${VERSION}_${OS}_${ARCH}"
   echo "Building ver. ${VERSION} for ${OS} (${ARCH}) to ${OUTPUT_DIR}..."
   mkdir ${OUTPUT_DIR}
-  env GOOS=${OS} GOARCH=${ARCH} go build -ldflags="-w -s -X main.builtAt=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -X main.version=${VERSION}" -o "${OUTPUT_DIR}/" github.com/jyggen/intro-detection-info
+  env GOOS=${OS} GOARCH=${ARCH} go build -trimpath -ldflags="-w -s -X main.builtAt=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -X main.version=${VERSION}" -o "${OUTPUT_DIR}/" github.com/jyggen/intro-detection-info
   cp CHANGELOG.md LICENSE README.md ${OUTPUT_DIR}/
   tar -czvf ${OUTPUT_DIR}.tar.gz ${OUTPUT_DIR}
   rm -rf ${OUTPUT_DIR}/
