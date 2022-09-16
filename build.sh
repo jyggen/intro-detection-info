11c6d2a4c0813b5d1b26af0fe36caeecb8d47295
Jonas Stendahl <jonas@stendahl.me>
2022-09-16 12:22:49 +0200

    Build for arm64 and store Windows build in zip

--- build.sh
+++ build.sh
@@ -9,10 +9,17 @@ build() {
   mkdir ${OUTPUT_DIR}
   env GOOS=${OS} GOARCH=${ARCH} go build -trimpath -ldflags="-w -s -X main.builtAt=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -X main.version=${VERSION}" -o "${OUTPUT_DIR}/" github.com/jyggen/intro-detection-info
   cp CHANGELOG.md LICENSE README.md ${OUTPUT_DIR}/
-  tar -czvf ${OUTPUT_DIR}.tar.gz ${OUTPUT_DIR}
+
+  if [ "${OS}" = "windows" ]; then
+    zip -r ${OUTPUT_DIR}.zip ${OUTPUT_DIR}
+  else
+    tar -czvf ${OUTPUT_DIR}.tar.gz ${OUTPUT_DIR}
+  fi
   rm -rf ${OUTPUT_DIR}/
 }
 
 build darwin amd64 $1
+build darwin arm64 $1
 build linux amd64 $1
+build linux arm64 $1
 build windows amd64 $1
